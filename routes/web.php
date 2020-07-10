f04cdf17aaa90bf1e0b53b21ff5f7e744ed1105c
Vannda <vandakol222@gmail.com>
2020-07-11 00:57:39 +0700

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 Route::resource('products', 'ProductController'); 
+Route::get('/menu', function () {
+    return view('menu');
+});
 
