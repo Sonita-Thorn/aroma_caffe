5c49a6c1a4afd886ad2b1d1023ea5769de89cc4c
Vannda <vandakol222@gmail.com>
2020-07-15 11:10:58 +0700

    last commit

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -63,7 +63,41 @@
                                                         <h3 class="mb-3">{{$products->price}}$.</h3>
                                                             <h5 class="text-upercase font-weight-light">{{$products->description}}</h5>
                                                
-                                                                <a href="#" class="btn hvr-right my-4 text-capitilize">Add to cart</a>
+                                                                <div class="container">
+                                        
+                                                                    <!-- Button to Open the Modal -->
+                                                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                                                                        Add to cart
+                                                                    </button>
+
+                                                                    <!-- The Modal -->
+                                                                    <div class="modal" id="myModal">
+                                                                        <div class="modal-dialog">
+                                                                        <div class="modal-content">
+                                                                        
+                                                                            <!-- Modal Header -->
+                                                                            <div class="modal-header">
+                                                                            <h4 class="modal-title">Purchase</h4>
+                                                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                                            </div>
+                                                                            
+                                                                            <!-- Modal body -->
+                                                                            <div class="modal-body">
+                                                                            Modal body..
+                                                                            </div>
+                                                                            
+                                                                            <!-- Modal footer -->
+                                                                            <div class="modal-footer">
+                                                                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                                                            <button type="button" class="btn btn-danger" >Summit</button>
+                                                                            </div>
+                                                                            
+                                                                        </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    
+                                                                    </div>
+
                                                     </div>
                                                 </div>
                                             </div>
